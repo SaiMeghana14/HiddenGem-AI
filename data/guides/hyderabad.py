a73ec6ec0863c432798a65e71ddc1b3cdba19855
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-08-20 20:12:09 +0530

    Create hyderabad.py

---
+++ data/guides/hyderabad.py
@@ -0,0 +1,7 @@
+def get_guide():
+    return {
+        "overview": "Hyderabad, the City of Pearls, blends Mughal heritage with IT hub modernity.",
+        "must_visit": ["Charminar", "Golconda Fort", "Hussain Sagar Lake"],
+        "local_food": ["Hyderabadi Biryani", "Haleem", "Irani Chai"],
+        "tips": "Best time is October to February. Bargain in Laad Bazaar."
+    }
